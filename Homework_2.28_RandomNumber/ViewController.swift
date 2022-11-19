5400c4261918b2cc63d9abe789187e3879860099
Ravil <ravilgab@yahoo.com>
2022-11-19 14:12:25 +0600

    Project created. Learning segues.

---
+++ Homework_2.28_RandomNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_2.28_RandomNumber
+//
+//  Created by Ravil on 19.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
